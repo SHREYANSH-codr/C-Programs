7bda3ba2e67c2d39270af633d5c116c9e9fe10e0
Shreyansh Yadav <89652665+SHREYANSH-codr@users.noreply.github.com>
2025-10-15 23:59:03 +0530

    Add files via upload

---
+++ Experiment-3.2/exp3.2_q3a.c
@@ -0,0 +1,29 @@
+//WAP to generate the following set of output.
+//a.   1
+//    2 3
+//   4 5 6
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int num = 1;       // Starting number
+    int rows = 3;      // Number of rows in the triangle
+
+    for(i = 1; i <= rows; i++) {
+        // Print spaces before numbers
+        for(j = 1; j <= rows - i; j++) {
+            printf(" ");
+        }
+
+        // Print numbers
+        for(j = 1; j <= i; j++) {
+            printf("%d ", num);
+            num++;
+        }
+
+        printf("\n");  // Move to next line
+    }
+
+    return 0;
+}
