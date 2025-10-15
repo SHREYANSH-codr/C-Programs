a1dc443e63a68573b718d2a663a34bc2d60a8837
Shreyansh Yadav <89652665+SHREYANSH-codr@users.noreply.github.com>
2025-10-15 23:01:12 +0530

    Add files via upload

---
+++ Experiment-1/exp1_q2.c
@@ -0,0 +1,10 @@
+//Write a C Program to print the address in multiple lines (new line)
+#include <stdio.h>
+
+int main() {
+    printf("Room No. 303\n");
+    printf("Upes Bidohli\n");
+    printf("Dehradun, Uttarakhand\n");
+    printf("India - 248001\n");
+    return 0;
+}
